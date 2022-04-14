e7aec5fc8226a2e4441e8f2b4eb43d4efc0b383a
icabiKrgz <70774175+icabiKrgz@users.noreply.github.com>
2022-04-15 00:59:55 +0300

    Component Architecture

--- 1_Basics/intro/src/App.js
+++ 1_Basics/intro/src/App.js
@@ -1,10 +1,25 @@
 // Icabi KIRGIZ / React
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
 
 function App() {
   return (
+    // React = Component Architecture
     <div>
+      {/* Components */}
+      <Navi></Navi>
+      <CategoryList></CategoryList>
+      <ProductList></ProductList>
+      <Navi></Navi>
+
+      <hr></hr>
+
+      {/* Public */}
       <h3>Hello From React</h3>
+      <h2>Hi !</h2>
     </div>
+    // <h2>Hi !</h2> it not working because it needs container tag (div)
   );
 }
 
---
+++ 1_Basics/intro/src/Navi.js
@@ -0,0 +1,12 @@
+// rcc 
+import React, { Component } from 'react'
+
+export default class Navi extends Component { // Class name = Pascal Case
+  render() {
+    return (
+      <div>
+          <h2>Navi Component</h2>
+      </div>
+    )
+  }
+}
